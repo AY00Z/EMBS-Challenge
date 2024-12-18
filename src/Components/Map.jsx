0f24758c31a696c01e0633bce674a86ad25b596a
salmabouaziz <salma.bouaziz@ensi-uma.tn>
2024-12-18 23:56:31 +0100

    map

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,53 +1,280 @@
-import React, { useRef, useEffect, useState } from 'react';
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-import './Map.css';
-import { MaptilerLayer } from "@maptiler/leaflet-maptilersdk";
-
-const Map = () => {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const center = { lng: 13.338414, lat: 52.507932 };
-  const [zoom] = useState(12);
-
-  useEffect(() => {
-    // Check if map is already initialized
-    if (map.current) return;
-
-    // Create map instance
-    map.current = L.map(mapContainer.current, {
-      center: L.latLng(center.lat, center.lng),
-      zoom: zoom,
-      attributionControl: false // Optional: removes default attribution
-    });
-
-    // Create MapTiler Layer
-    try {
-      // eslint-disable-next-line no-unused-vars
-      const mtLayer = new MaptilerLayer({
-        apiKey: "Y4kL9nNNmU8RU2ZNYKmjz",
-        style: 'streets', // or another style like 'basic', 'bright', 'pastel'
-      }).addTo(map.current);
-
-      // Optional: Add some console logs for debugging
-      console.log('Map initialized successfully');
-    } catch (error) {
-      console.error('Error initializing MapTiler layer:', error);
-    }
-
-    // Cleanup function
-    return () => {
-      if (map.current) {
-        map.current.remove();
-      }
-    };
-  }, [center.lng, center.lat, zoom]);
+import React, { useState }  from 'react';
+import MapPin from "./map-removebg-preview.png"
+import './Map.css'
+const MAP = () => {
+ 
+  const [showImage, setShowImage] = useState(false);
+  const handleSearch = () => {
+    setShowImage(true); 
+  };
+  
+  
+  const [isOpen1, setIsOpen1] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+  const toggleModal1 = () => {
+    setIsOpen1(!isOpen1);
+  };
+  const toggleModal2 = () => {
+    setIsOpen2(!isOpen2);
+  };
 
   return (
-    <div className="map-wrap">
-      <div ref={mapContainer} className="map" />
+    
+    <div style={{ width: '100%', height: '92vh',marginTop:'8vh'}}>
+
+      {showImage && (
+        <div 
+          
+          style={{
+          position:"absolute",
+          marginLeft: '250px',
+          marginTop:'500px',
+          cursor:'pointer'
+           // Ajustez la taille selon vos besoins
+        }}>
+          <button
+          onClick={toggleModal1}>
+            <h3 className="hover-container" ><b> 
+            Docteur Mohamed Ben Saleh </b></h3>
+        </button>
+           
+          <img 
+          src={MapPin} // Remplacez par l'URL de votre image
+          alt="Logo"
+          style={{width: '30px',
+            height: '30px',}}
+        />
+        </div>
+      )}
+
+      <div style={{  textAlign: 'center',position:'absolute',marginTop:'2vh',marginLeft:'35%' }}>
+        <input
+          type="text"
+          placeholder="Search..."
+          style={{
+            padding: '10px',
+            width: '300px',
+            fontSize: '16px',
+            borderRadius: '5px',
+            border: '1px solid #ccc',
+          }}
+        />
+        <button
+          onClick={handleSearch}
+          style={{
+            padding: '10px 20px',
+            fontSize: '16px',
+            marginLeft: '10px',
+            cursor: 'pointer',
+            borderRadius: '5px',
+            border: '1px solid #ccc',
+            backgroundColor: '#4CAF50',
+            color: 'white',
+          }}
+        >
+          Search
+        </button>
+    </div>
+        <iframe 
+          width="100%" 
+          height="645" 
+          frameBorder="0" 
+          scrolling="no" 
+          marginHeight="0" 
+          marginWidth="0" 
+          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=cabinet%20+(docteur)&amp;t=p&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+          title="Google Map"
+        >
+        </iframe>
+
+        {isOpen1 && (
+        <div
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fond semi-transparent
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            zIndex: 1000,
+          }}
+        >
+          <div
+            style={{
+              background: 'white',
+              padding: '20px',
+              borderRadius: '10px',
+              width: '400px',
+              textAlign: 'center',
+              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+            }}
+          >
+            <h2><b>Book a medical visit</b></h2>
+            <form >
+              {/* Champ Nom */}
+              <div style={{ marginBottom: '10px', textAlign: 'left' }}>
+                <label htmlFor="name">Full Name :</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  required
+                  style={{
+                    width: '100%',
+                    padding: '8px',
+                    margin: '5px 0',
+                    border: '1px solid #ccc',
+                    borderRadius: '5px',
+                  }}
+                />
+              </div>
+
+              {/* Champ Email */}
+              <div style={{ marginBottom: '10px', textAlign: 'left' }}>
+                <label htmlFor="email">Email :</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  required
+                  style={{
+                    width: '100%',
+                    padding: '8px',
+                    margin: '5px 0',
+                    border: '1px solid #ccc',
+                    borderRadius: '5px',
+                  }}
+                />
+              </div>
+
+              {/* Champ Date */}
+              <div style={{ marginBottom: '10px', textAlign: 'left' }}>
+                <label htmlFor="date">Date :</label>
+                <input
+                  type="date"
+                  id="date"
+                  name="date"
+                  required
+                  style={{
+                    width: '100%',
+                    padding: '8px',
+                    margin: '5px 0',
+                    border: '1px solid #ccc',
+                    borderRadius: '5px',
+                  }}
+                />
+              </div>
+
+              {/* Champ Heure */}
+              <div style={{ marginBottom: '10px', textAlign: 'left' }}>
+                <label htmlFor="time">Time :</label>
+                <input
+                  type="time"
+                  id="time"
+                  name="time"
+                  required
+                  style={{
+                    width: '100%',
+                    padding: '8px',
+                    margin: '5px 0',
+                    border: '1px solid #ccc',
+                    borderRadius: '5px',
+                  }}
+                />
+              </div>
+
+              {/* Boutons */}
+              <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
+                <button
+                  onClick={toggleModal1}
+                  type="button"
+                  style={{
+                    padding: '10px 20px',
+                    fontSize: '16px',
+                    cursor: 'pointer',
+                    backgroundColor: '#f44336',
+                    color: 'white',
+                    border: 'none',
+                    borderRadius: '5px',
+                  }}
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={() => {
+                    toggleModal1();
+                    toggleModal2();
+                  }}
+                  type="submit"
+                  style={{
+                    padding: '10px 20px',
+                    fontSize: '16px',
+                    cursor: 'pointer',
+                    backgroundColor: '#4CAF50',
+                    color: 'white',
+                    border: 'none',
+                    borderRadius: '5px',
+                  }}
+                >
+                  Apply
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+
+
+      {isOpen2 && (
+        <div
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fond semi-transparent
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            zIndex: 1000,
+          }}
+        >
+          <div
+            style={{
+              background: 'white',
+              padding: '20px',
+              borderRadius: '10px',
+              width: '400px',
+              textAlign: 'center',
+              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+            }}
+          >
+            <h2><b>Form submitted successfully ! </b>  </h2>
+            <button
+              onClick={toggleModal2}
+              style={{
+                padding: '5px 15px',
+                fontSize: '16px',
+                cursor: 'pointer',
+                backgroundColor: '#276221',
+                color: 'white',
+                border: 'none',
+                borderRadius: '5px',
+                marginTop: '20px',
+              }}
+            >
+               Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
+export default MAP;
 
-export default Map;
+
